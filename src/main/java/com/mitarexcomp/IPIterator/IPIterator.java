10281db0b43648fe0d3f085a18670c8de6682d8f
Mitarexia <mitarex@gmail.com>
2016-12-23 04:08:40 +0300

    Importing the IPIterator project

---
+++ src/main/java/com/mitarexcomp/IPIterator/IPIterator.java
@@ -0,0 +1,42 @@
+package com.mitarexcomp.IPIterator;
+
+/**
+ * Hello world!
+ *
+ */
+public class IPIterator 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "No code yet!" );
+    }
+    
+    // Метод для преобразования строки с IP адресом в long
+    public static long ipToLong(String ipAddress) {
+        long result = 0;
+        String[] atoms = ipAddress.split("\\.");
+
+        for (int i = 3; i >= 0; i--) {
+            result |= (Long.parseLong(atoms[3 - i]) << (i * 8));
+        }
+
+        return result & 0xFFFFFFFF;
+    }
+	
+    // Метод для преобразования IP адреса в строку
+    public static String longToIp(long ip) {
+        StringBuilder sb = new StringBuilder(15);
+
+        for (int i = 0; i < 4; i++) {
+            sb.insert(0, Long.toString(ip & 0xff));
+
+            if (i < 3) {
+                sb.insert(0, '.');
+            }
+
+            ip >>= 8;
+        }
+
+        return sb.toString();
+    }
+}
